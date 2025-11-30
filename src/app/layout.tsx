b2d73990873a83aeddeb8427175ea74ae3361203
loqman1235 <tonyvito12@gmail.com>
2025-11-30 19:46:41 +0100

    chore: initial project setup with shadcn/ui

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter, Instrument_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter-sans",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+const instrument = Instrument_Serif({
+  variable: "--font-instrument-serif",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const metadata: Metadata = {
+  title: "Rattl.",
+  description: "A social media website.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${inter.variable} ${instrument.variable}`}>
+      <body className="antialiased">{children}</body>
+    </html>
+  );
+}
