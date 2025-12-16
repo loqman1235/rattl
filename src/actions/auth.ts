b9aac3690ef6da1fdb62363c90aaf60de8c7a2b2
loqman1235 <tonyvito12@gmail.com>
2025-12-16 10:06:31 +0100

    feat: create auth actions

---
+++ src/actions/auth.ts
@@ -0,0 +1,14 @@
+"use server";
+import { auth } from "@/lib/auth/config";
+import { cookies, headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function signOut() {
+  await auth.api.signOut({
+    headers: await headers(),
+  });
+
+  (await cookies()).delete("better-auth.session_token");
+
+  redirect("/");
+}
