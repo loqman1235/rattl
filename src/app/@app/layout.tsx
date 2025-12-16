f1a8b0aa951d6d5b53e0243a6fa94d619ff4a126
loqman1235 <tonyvito12@gmail.com>
2025-12-16 10:37:54 +0100

    feat: add session verfication to app layout

--- src/app/@app/layout.tsx
+++ src/app/@app/layout.tsx
@@ -1,4 +1,12 @@
-const AppLayout = ({ children }: { children: React.ReactNode }) => {
+import { getSession } from "@/lib/auth/session";
+import { redirect } from "next/navigation";
+
+const AppLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getSession();
+
+  if (!session?.user) {
+    redirect("/");
+  }
   return (
     <main>
       <div className="min-h-screen">{children}</div>
