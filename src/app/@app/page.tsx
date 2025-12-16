605f43637fb96ad1c1c6befb0d24e66b764038ad
loqman1235 <tonyvito12@gmail.com>
2025-12-16 10:38:25 +0100

    feat: add app page with temporary ui

--- src/app/@app/page.tsx
+++ src/app/@app/page.tsx
@@ -1,4 +1,25 @@
-const AppPage = () => {
-  return <div>AppPage</div>;
+import { signOut } from "@/actions/auth";
+import { Button } from "@/components/ui/button";
+import { getSession } from "@/lib/auth/session";
+import { redirect } from "next/navigation";
+
+const AppPage = async () => {
+  const session = await getSession();
+
+  if (!session?.user) {
+    redirect("/");
+  }
+
+  // TODO: Work on main app
+  return (
+    <div className="container mx-auto p-4 flex flex-col gap-10">
+      <h1 className="text-3xl font-bold">Feed</h1>
+      <p>Welcome, @{session.user.username}!</p>
+
+      <form action={signOut}>
+        <Button type="submit">Sign out</Button>
+      </form>
+    </div>
+  );
 };
 export default AppPage;
