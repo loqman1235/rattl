7fb90056b4d8c9ca51d47b01d379f36d98d6cbb3
loqman1235 <tonyvito12@gmail.com>
2025-12-16 10:35:48 +0100

    feat: update landing layout and add session verification

--- src/app/@landing/layout.tsx
+++ src/app/@landing/layout.tsx
@@ -1,9 +1,17 @@
+import { getSession } from "@/lib/auth/session";
 import { Footer } from "./_components/footer";
+import { redirect } from "next/navigation";
 
-const LandingLayout = ({ children }: { children: React.ReactNode }) => {
+const LandingLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getSession();
+
+  if (session?.user) {
+    redirect("/");
+  }
+
   return (
-    <div className="px-10 py-5">
-      <main className="mb-5">{children}</main>
+    <div className="px-10 py-5 flex flex-col min-h-screen gap-20">
+      <main className="flex-1 grid place-items-center">{children}</main>
       <Footer />
     </div>
   );
