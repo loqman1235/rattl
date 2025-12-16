668174ff16d69d79dd1880074ec58884b14c9253
loqman1235 <tonyvito12@gmail.com>
2025-12-16 10:27:41 +0100

    feat: add nodemailer sendEmail helper function

---
+++ src/lib/email.ts
@@ -0,0 +1,33 @@
+// create an email sending function with nodemailer
+
+import nodemailer from "nodemailer";
+
+export async function sendEmail({
+  to,
+  subject,
+  text,
+  html,
+}: {
+  to: string;
+  subject: string;
+  text: string;
+  html?: string;
+}) {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: Number(process.env.EMAIL_PORT),
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  await transporter.sendMail({
+    from: process.env.EMAIL,
+    to,
+    subject,
+    text,
+    html,
+  });
+}
