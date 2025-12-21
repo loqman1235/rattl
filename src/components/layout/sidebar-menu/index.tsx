1933818f238e52419d72dfdc2851d9e2ebad69c3
loqman1235 <tonyvito12@gmail.com>
2025-12-21 07:00:34 +0100

    feat: implement SidebarMenu component with user authentication and layout

---
+++ src/components/layout/sidebar-menu/index.tsx
@@ -0,0 +1,37 @@
+import { Brand } from "@/components/shared/brand";
+import { SidebarMenuNav } from "./navigation";
+import { ProfileButton } from "./profile-button";
+import { getCurrentUser } from "@/lib/auth/session";
+import { PostButton } from "./post-button";
+
+export const SidebarMenu = async () => {
+  const user = await getCurrentUser();
+
+  if (!user) return null;
+
+  return (
+    <aside className="w-[var(--sidebar-menu-width-mobile)] md:w-[var(--sidebar-menu-width-mobile)] lg:w-[var(--sidebar-menu-width)] transition-all duration-300 ease-in-out border-r border-r-border">
+      <div className="sticky top-5 flex flex-col gap-4 min-h-screen py-5">
+        <div className="flex-1 flex flex-col gap-2">
+          {/* LOGO */}
+          <div className="px-9">
+            <Brand />
+          </div>
+
+          {/* NAVIGATION */}
+          <SidebarMenuNav />
+
+          {/* POST BUTTON */}
+          <div className="px-6 flex items-center justify-center">
+            <PostButton />
+          </div>
+        </div>
+
+        {/* PROFILE BUTTON */}
+        <div className="px-3 md:px-6 mt-auto flex items-center justify-center w-full">
+          <ProfileButton user={user} />
+        </div>
+      </div>
+    </aside>
+  );
+};
