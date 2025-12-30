e72d7bc12d45c95c5966cb217d35c15c07f77e20
loqman1235 <tonyvito12@gmail.com>
2025-12-30 12:36:01 +0100

    feat: rename AppPage to FeedPage and update layout with navigation and post components

--- src/app/@app/page.tsx
+++ src/app/@app/page.tsx
@@ -1,7 +1,9 @@
+import { PostCard } from "@/components/post/post-card";
+import { PostComposer } from "@/components/post/post-composer";
 import { getSession } from "@/lib/auth/session";
 import { redirect } from "next/navigation";
 
-const AppPage = async () => {
+const FeedPage = async () => {
   const session = await getSession();
 
   if (!session?.user) {
@@ -12,11 +14,28 @@ const AppPage = async () => {
     redirect("/onboarding/username");
   }
 
-  // TODO: Work on main app
+  // TODO: Work on feed
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Feed</h1>
+    <div className="pb-20">
+      {/* HEADER */}
+      <nav className="flex sticky top-0 z-10">
+        <button className="w-1/2 p-3 flex items-center justify-center border-b border-b-border bg-background/80 backdrop-blur-sm font-semibold hover:bg-secondary cursor-pointer text-primary transition-all duration-300 ease-in-out relative after:absolute after:bottom-0 after:w-[100px] after:h-1 after:bg-primary after:rounded-full after:transition-all after:duration-300 after:ease-in-out">
+          Discover
+        </button>
+        <button className="w-1/2 p-3 flex items-center justify-center border-b border-b-border bg-background/80 backdrop-blur-sm font-semibold hover:bg-secondary cursor-pointer text-muted-foreground transition-all duration-300 ease-in-out">
+          Following
+        </button>
+      </nav>
+      {/* POST INPUT */}
+      <PostComposer user={session.user} />
+      {/* FEED POSTS */}
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
     </div>
   );
 };
-export default AppPage;
+export default FeedPage;
