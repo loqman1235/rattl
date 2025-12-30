e448e2bbbd635f1ce4f33ca1d2313e1c79d4306b
loqman1235 <tonyvito12@gmail.com>
2025-12-30 12:36:34 +0100

    feat: add ComposePostModal component for post creation with user context

---
+++ src/components/modals/compose-post-modal.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { XIcon } from "lucide-react";
+import { Dialog, DialogContent, DialogTitle } from "../ui/dialog";
+import { PostComposer } from "../post/post-composer";
+import { useRouter } from "next/navigation";
+import { User } from "better-auth";
+
+type ComposePostModalProps = {
+  user: User | null;
+};
+
+export const ComposePostModal = ({ user }: ComposePostModalProps) => {
+  const router = useRouter();
+  const handleClose = () => {
+    router.back();
+  };
+  return (
+    <Dialog open={true} onOpenChange={handleClose}>
+      <DialogContent className="sm:max-w-[600px] [&>button]:hidden !p-0 !gap-0">
+        <div className="flex items-center justify-between pt-4 pl-4 ">
+          <DialogTitle className="sr-only" />
+          <button
+            className="rounded-full p-2 flex items-center justify-center hover:bg-secondary transition-all cursor-pointer"
+            onClick={handleClose}
+          >
+            <XIcon className="size-5" />
+          </button>
+        </div>
+
+        <PostComposer user={user} initialHeight={80} />
+      </DialogContent>
+    </Dialog>
+  );
+};
