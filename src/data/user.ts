94275b3f6815e162301a6b046914dbec98848106
loqman1235 <tonyvito12@gmail.com>
2026-01-13 11:19:44 +0100

    feat: add user data object for profile information

---
+++ src/data/user.ts
@@ -0,0 +1,16 @@
+export const user = {
+  name: "Loqman Djefafla",
+  isVerified: true,
+  bio: "Developer of Rattl",
+  location: "Algeria",
+  website: "https://loqmanedjefafla.com",
+  image: "",
+  username: "loqmanedj",
+  id: "1",
+  stats: {
+    followers: 1244458,
+    following: 5,
+    posts: 5,
+  },
+  createdAt: new Date(Date.now() - 1000 * 60 * 60 * 2),
+};
