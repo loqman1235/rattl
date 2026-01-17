1d477f6346bbce3574a13c9a571babed8f42acbd
loqman1235 <tonyvito12@gmail.com>
2026-01-17 13:22:46 +0100

    fix: remove react-icons icons from google-button and github-button

--- src/app/@landing/_components/google-button.tsx
+++ src/app/@landing/_components/google-button.tsx
@@ -1,10 +1,8 @@
-import { FcGoogle } from "react-icons/fc";
 import { Button } from "@/components/ui/button";
 
 export const GoogleButton = () => {
   return (
     <Button size="lg" variant="outline">
-      <FcGoogle className="size-5" />
       Sign up with Google
     </Button>
   );
