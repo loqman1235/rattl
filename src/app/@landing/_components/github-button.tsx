d264119a0b7a5937cf9caef3c7f4e043ed3ecbae
loqman1235 <tonyvito12@gmail.com>
2026-01-17 13:22:12 +0100

    fix: remove react-icons icons

--- src/app/@landing/_components/github-button.tsx
+++ src/app/@landing/_components/github-button.tsx
@@ -1,11 +1,8 @@
-import { FaGithub } from "react-icons/fa6";
-
 import { Button } from "@/components/ui/button";
 
 export const GithubButton = () => {
   return (
     <Button size="lg" variant="outline">
-      <FaGithub className="size-5" />
       Sign up with Github
     </Button>
   );
