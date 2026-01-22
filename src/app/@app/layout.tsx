92ac3806390019676257006c3bb333d348505905
loqman1235 <tonyvito12@gmail.com>
2026-01-22 12:47:24 +0100

    feat: replace static right sidebar with RightSidebar component

--- src/app/@app/layout.tsx
+++ src/app/@app/layout.tsx
@@ -1,3 +1,4 @@
+import { RightSidebar } from "@/components/layout/right-sidebar";
 import { SidebarMenu } from "@/components/layout/sidebar-menu";
 import { FloatingPostButton } from "@/components/layout/sidebar-menu/floating-post-button";
 import { MobileNav } from "@/components/layout/sidebar-menu/mobile-nav";
@@ -19,9 +20,7 @@ const AppLayout = async ({ children }: { children: React.ReactNode }) => {
       <div className="flex-1 min-w-0">{children}</div>
 
       {/* Right sidebar */}
-      <aside className="hidden xl:block w-[var(--right-sidebar-width)] transition-all duration-300 ease-in-out border-l border-l-border p-5">
-        <div className="sticky top-5">Right sidebar</div>
-      </aside>
+      <RightSidebar />
 
       {/* MOBILE BOTTOM NAV - MOBILE ONLY */}
       <div className="md:hidden">
