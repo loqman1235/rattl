4a3ce277e81149d02147a6dccd1845c7386e8eaa
loqman1235 <tonyvito12@gmail.com>
2026-01-29 12:21:44 +0100

    refactor: remove unused GoogleButton component

--- src/app/@landing/_components/google-button.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export const GoogleButton = () => {
-  return (
-    <Button size="lg" variant="outline">
-      Sign up with Google
-    </Button>
-  );
-};
