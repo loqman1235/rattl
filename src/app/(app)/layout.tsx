75c4c1fd2078d192655a4022f54909a2ab18ec54
loqman1235 <tonyvito12@gmail.com>
2026-01-29 12:15:15 +0100

    feat: implement AppLayout component with sidebar and navigation

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,35 @@
+import { RightSidebar } from "@/components/layout/right-sidebar";
+import { SidebarMenu } from "@/components/layout/sidebar-menu";
+import { FloatingPostButton } from "@/components/layout/sidebar-menu/floating-post-button";
+import { MobileNav } from "@/components/layout/sidebar-menu/mobile-nav";
+import { getSession } from "@/lib/auth/session";
+import { redirect } from "next/navigation";
+
+const AppLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getSession();
+
+  if (!session?.user) {
+    redirect("/");
+  }
+
+  return (
+    <main className="flex max-w-7xl mx-auto min-h-screen">
+      <SidebarMenu />
+
+      {/* Main content */}
+      <div className="flex-1 min-w-0">{children}</div>
+
+      {/* Right sidebar */}
+      <RightSidebar />
+
+      {/* MOBILE BOTTOM NAV - MOBILE ONLY */}
+      <div className="md:hidden">
+        <MobileNav user={session.user} />
+      </div>
+
+      {/* POST BUTTON - MOBILE ONLY */}
+      <FloatingPostButton />
+    </main>
+  );
+};
+export default AppLayout;
