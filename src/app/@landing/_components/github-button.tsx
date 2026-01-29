99d359209e699ee25dee46740648c3a3336339a1
loqman1235 <tonyvito12@gmail.com>
2026-01-29 12:21:55 +0100

    refactor: remove unused GithubButton component

--- src/app/@landing/_components/github-button.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export const GithubButton = () => {
-  return (
-    <Button size="lg" variant="outline">
-      Sign up with Github
-    </Button>
-  );
-};
